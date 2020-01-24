75a8e631d5be8b7950d8dcdffbb08a6883cf5eae
Toni Bolduan <toni.bolduan@googlemail.com>
2020-01-24 18:48:30 +0100

    add main.cpp & concept as comment

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <crypt.h>
+
+int main () {
+
+    
+
+    // create list of files & folders
+    /*
+        <file1>                 <HASH>
+            <block1>                <HASH>
+            <block2>                <HASH>
+            ...
+            <blockN>                <HASH>
+
+        ...
+
+        <fileN>                 <HASH>
+
+
+        /path/to/file1.dat      AE4C12B36
+            1                        FFE12B6                          
+            2                        C4FE1D5
+            ...
+            N                       <NNNNNNN>
+
+        /path/to/file2.dat      B3E423F28
+
+    */
+   
+
+
+}
+
+
